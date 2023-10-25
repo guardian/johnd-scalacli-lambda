cfbf0a6319c7f1e96b0ae2ebb7c958c1a5052db2
John Duffell <john.duffell@guardian.co.uk>
2023-10-25 14:53:36 +0100

    make lambda speak proper API gateway language

--- scala/src/HelloWorld.scala
+++ scala/src/HelloWorld.scala
@@ -1,38 +1,48 @@
 //> using toolkit latest
 //> using dep com.amazonaws:aws-lambda-java-core:1.2.3
+//> using dep com.amazonaws:aws-lambda-java-events:3.11.3
 
+import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
 import com.amazonaws.services.lambda.runtime.{ClientContext, CognitoIdentity, Context, LambdaLogger, RequestHandler}
-// > using dep com.amazonaws:aws-lambda-java-events:3.11.3
+import scala.jdk.CollectionConverters.*
 
 @main
 def main():Unit =
   val jsonString = """{"name": "Peter", "age": 13, "pets": ["Toolkitty", "Scaniel"]}"""
   val json: ujson.Value  = ujson.read(jsonString)
   println(json("name").str)
-  println(Handler.handleRequest("cheese", new Context() {
-    override def getAwsRequestId: String = ???
+  val testInput = new APIGatewayProxyRequestEvent()
+    .withBody("cheese")
+  println(Handler.handleRequest(testInput, dummyContext))
 
-    override def getLogGroupName: String = ???
+object Handler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent]:
+  override def handleRequest(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent =
+    new APIGatewayProxyResponseEvent()
+      .withStatusCode(200)
+      .withHeaders(Map("Content-type" -> "text/plain").asJava)
+      .withBody("Hello " + input.getBody)
 
-    override def getLogStreamName: String = ???
 
-    override def getFunctionName: String = ???
+val dummyContext = new Context() {
+  override def getAwsRequestId: String = ???
 
-    override def getFunctionVersion: String = ???
+  override def getLogGroupName: String = ???
 
-    override def getInvokedFunctionArn: String = ???
+  override def getLogStreamName: String = ???
 
-    override def getIdentity: CognitoIdentity = ???
+  override def getFunctionName: String = ???
 
-    override def getClientContext: ClientContext = ???
+  override def getFunctionVersion: String = ???
 
-    override def getRemainingTimeInMillis: Int = ???
+  override def getInvokedFunctionArn: String = ???
 
-    override def getMemoryLimitInMB: Int = ???
+  override def getIdentity: CognitoIdentity = ???
 
-    override def getLogger: LambdaLogger = ???
-  }))
+  override def getClientContext: ClientContext = ???
 
-object Handler extends RequestHandler[String, String]:
-  override def handleRequest(input: String, context: Context): String =
-    s"Hello, $input!"
+  override def getRemainingTimeInMillis: Int = ???
+
+  override def getMemoryLimitInMB: Int = ???
+
+  override def getLogger: LambdaLogger = ???
+}
